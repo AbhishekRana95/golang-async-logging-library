a05bb539b1f272b86083fddf80f07e24c28e18bb
Abhishek Rana <rana.abhishek.official@gmail.com>
2021-06-13 17:01:13 +0530

    Completed module

--- alog.go
+++ alog.go
@@ -13,6 +13,7 @@ import (
 
 // Alog is a type that defines a logger. It can be used to write log messages synchronously (via the Write method)
 // or asynchronously via the channel returned by the MessageChannel accessor.
+
 type Alog struct {
 	dest               io.Writer
 	m                  *sync.Mutex
@@ -33,18 +34,27 @@ func New(w io.Writer) *Alog {
 		m: &sync.Mutex{},
 		msgCh: make(chan string),
 		errorCh: make(chan error),
+		shutdownCh: make(chan struct{}),
+		shutdownCompleteCh: make(chan struct{}, 1),
 	}
 }
 
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
-	for {
-		select {
-		case log := <- al.msgCh:
-			go al.write(log, nil)
+	wg:= sync.WaitGroup{}
+	MainLoop:
+		for {
+			select {
+			case log := <- al.msgCh:
+				wg.Add(1)
+				go al.write(log, &wg)
+			case <- al.shutdownCh:
+				wg.Wait()
+				go al.shutdown()
+				break MainLoop
+			}
 		}
-	}
 }
 
 func (al Alog) formatMessage(msg string) string {
@@ -59,6 +69,7 @@ func (al Alog) write(msg string, wg *sync.WaitGroup) {
 	defer al.m.Unlock()
 	_, err := al.Write(msg)
 	go al.sendError(err)
+	wg.Done()
 }
 
 func (al Alog) sendError(err error) {
@@ -68,6 +79,8 @@ func (al Alog) sendError(err error) {
 }
 
 func (al Alog) shutdown() {
+	close(al.msgCh)
+	al.shutdownCompleteCh <- struct{}{}
 }
 
 // MessageChannel returns a channel that accepts messages that should be written to the log.
@@ -85,6 +98,8 @@ func (al Alog) ErrorChannel() <-chan error {
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
 // The logger will no longer function after this method has been called.
 func (al Alog) Stop() {
+	al.shutdownCh <- struct{}{}
+	<- al.shutdownCompleteCh
 }
 
 // Write synchronously sends the message to the log output
